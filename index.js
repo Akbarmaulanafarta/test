8fdaa84a0dfcd92a23f3a8c0c0eeadcf23e98f23
Akbarmaulanafarta <138547403+Akbarmaulanafarta@users.noreply.github.com>
2023-09-30 09:31:59 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let nama = "akbar maulana farta";
+let tabungan = 100 ;
+let hutang = 50 ;
+let tunai = (tabungan * hutang);
+ console.log('perkenalkan nama saya ' + nama + 'dan tabungan saya sekitar ' + tunai);
+ 
+  
+
+ let hasil='70';
+let lulus='70';
+ if (hasil=>90) {
+  (lulus =='Selamat! Anda mendapatkan nilai A.');
+  
+ } else  if(hasil=>80 && hasil<=89) {
+  (lulus ='Selamat! Anda mendapatkan nilai B.');
+ 
+ }
+ else if(hasil=>70 && hasil<=79) {
+  (lulus ='Selamat! Anda mendapatkan nilai C.');
+ 
+ }
+ else if (hasil=>60 && hasil<=69) {
+  (lulus ='Selamat! Anda mendapatkan nilai D.');
+ 
+ }
+ else if (hasil<=60) {
+  (lulus ='Selamat! Anda mendapatkan nilai E.');
+ 
+ }
+
+ console.log(lulus);
